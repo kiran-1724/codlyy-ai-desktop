01c03766b7e47f6d6103803356cd6e430e69bff1
kiran-1724 <kiran.s.devv@gmail.com>
2026-02-02 14:27:54 +0530

    feat: initial project setup with electron configuration and app icon

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1100,
+    height: 750,
+    autoHideMenuBar: true, // This removes the "File, Edit, View" menu
+    backgroundColor: '#ffffff',
+    icon: path.join(__dirname, 'assets/icon.ico'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
