8ce96f1bdb1443a29cddf9ce8db8fa6216a8762b
kiran-1724 <kiran.s.devv@gmail.com>
2026-02-02 23:29:01 +0530

    feat: Add auto-updater with liquid animation, version display, and What's New modal

--- preload.js
+++ preload.js
@@ -1,5 +1,15 @@
-const { contextBridge } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('codlyy', {
-  ping: () => 'pong'
+  ping: () => 'pong',
+  getAppVersion: () => ipcRenderer.invoke('get-app-version'),
+
+
+  // Auto-update API
+  onUpdateAvailable: (callback) => ipcRenderer.on('update-available', (event, value) => callback(value)),
+  onUpdateProgress: (callback) => ipcRenderer.on('update-progress', (event, value) => callback(value)),
+  onUpdateDownloaded: (callback) => ipcRenderer.on('update-downloaded', (event, value) => callback(value)),
+
+  startDownload: () => ipcRenderer.send('start-download'),
+  restartApp: () => ipcRenderer.send('restart-app')
 });
